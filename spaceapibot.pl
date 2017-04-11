b58e95aaef8b76a33eceb81c5bad57e0ba4bc60b
L3D <l3d@see-base.de>
2017-04-11 16:10:46 +0200

    Its a work in progress

--- spaceapibot.pl
+++ spaceapibot.pl
@@ -17,6 +17,7 @@ use Irssi;                      # Für den Bot
 use Config::IniFiles;           # From CPAN
 use LWP::Simple;                # From CPAN
 use JSON qw( decode_json );     # From CPAN
+use Switch;
 use vars qw($VERSION %IRSSI);
 $VERSION = "1.0";
 %IRSSI = (
@@ -31,9 +32,10 @@ $VERSION = "1.0";
 ## important global Variables:
 # For the IRC:
 our $main_channel = "see-base|ponyville"; #    Use '|' for multiple channels
-our $keyword = "space";
-our $open = "open"; # to change state to open
-our $closed = "closed"; # to change state to closed
+our @keyword = ("space", "see-base", "toolbox"); # First parameter is a for the default space!
+our @open = ("$keyword[0] open|$keyword[1] open", "$keyword[2] open"); # to change state to open
+our @closed = ("$keyword[0] closed|$keyword[1] closed", "$keyword[2] closed"); # to change state to closed
+our @show = ("$keyword[0]|$keyword[1]", "$keyword[2]");
 # Global Parameters:
 our $url = "https://bodensee.space/cgi-bin/togglestate?";
 
@@ -48,26 +50,49 @@ sub sig_message_public {
     my ($server, $msg, $nick, $nick_addr, $target) = @_;
     if ($target =~ m/#(?:$main_channel)/) { # only operate in these channels
         # listen to keyword to do something:
-        if ($msg =~ m/!(?:$keyword $open)/i){ # listening for "!$keyword"
-            my $openurl = "$url";
-            $server->command("msg $target Hey $nick, du wolltest den Space auf 'offen' stellen.");
-        }elsif ($msg =~ m/!(?:$keyword $closed)/i){ # listening for "!$keyword"
-            $server->command("msg $target Hey $nick, du wolltest den Space auf 'geschlossen' stellen.");
-        }elsif ($msg =~ m/!(?:$keyword)/i){ # listening for "!$keyword"
-            $server->command("msg $target Hey $nick, du wolltest den Status des Space rausfinden!");
+        my $int = 0;
+        foreach (@open){
+            if ($msg =~ m/!(?:$open[$int])/i){ # listening for "!$keyword open"
+                my $answer = space("open", $keyword[$int+1]);
+                $server->command("msg $target Hey $nick, $answer.");
+            }elsif ($msg =~ m/!(?:$closed[$int])/i){ # listening for "!$keyword closed"
+                my $answer = space("closed", $keyword[$int+1]);
+                $server->command("msg $target Hey $nick, $answer.");
+            }elsif ($msg =~ m/!(?:$show[$int])/i){ # listening for "!$keyword"
+                my $answer = space("show", $keyword[$int+1]);
+                $server->command("msg $target Hey $nick, $answer");
+            }
+            $int++;
         }
     }
     $server->command("/script load spaceapibot.pl");
 }
 
 sub space {
+    my $parameter = $_[0];
+    my $param2 = $_[1];
     my $anz = $ini->val('spaces', 'anzahl');
     my @spaces = [];
+    my @token = [];
     for (my $i = 0; $i < $anz; $i++) {
         push(@spaces, $ini->val('spaces', "space_$i"));
-    } 
-    print $spaces[1];
-
+        push(@token, $ini->val('token', "space_$i"));
+    }
+    for (my $i = 0; $i < $anz; $i++) {
+        if ($spaces[$i] eq $param2){
+            if ($parameter eq "open") {
+                my $link = "$url$spaces[$i]&token=$token[$i]&state=$parameter";
+                return "try to open";
+            }
+            elsif ($parameter eq "closed") {
+                return "try to close";
+            }
+            elsif ($parameter eq "show") {
+                return "try to show state";
+            }
+            else { return "Error: Ungueltige Parameteruebergabe"; }
+        }
+    }
+    return "foo";
 }
 
-space();
